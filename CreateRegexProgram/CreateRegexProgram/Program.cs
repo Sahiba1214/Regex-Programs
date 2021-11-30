feb39cddd84e62e4ff2399e438ca7226d3ae1744
sahiba <93565881+Sahiba1214@users.noreply.github.com>
2021-11-30 21:31:30 +0530

    Update Program.cs

--- CreateRegexProgram/CreateRegexProgram/Program.cs
+++ CreateRegexProgram/CreateRegexProgram/Program.cs
@@ -9,29 +9,50 @@ namespace CreateRegexProgram
         {
             bool flag = true;
             while (flag)
+            { UserRegistration validation = new UserRegistration();
+            bool flag = true;
+            while(flag)
             {
-                Console.WriteLine("Choose Option to Execute the Regex Program: \n 1. Validate First Name, \n 2. Exit");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
+                Console.WriteLine("Welcome to the User Registration validation program");
+                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. Last Name \n 3. Email ID \n 4. Mobile \n 5. Password Rule-1 \n 6. Password Rule-2 \n 7. Password Rule-3 \n 8. Password Rule-4 \n 9. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
                 {
                     case 1:
-                        Console.WriteLine("User Registration Validate using Regular Expression");
-
-                        RegexPatterns checkpattern = new RegexPatterns();
-                        Console.WriteLine("For Check Validation Enter First Name");
-                        string input = Console.ReadLine();
-                        checkpattern.ValidateFirstName(input);
-                        checkpattern.Validating();
+                        Console.WriteLine(validation.ValidateFirstName("Sahiba"));
                         break;
-
                     case 2:
+                        Console.WriteLine(validation.ValidateLastName("Fatma"));
+                        break;
+                    case 3:
+                        Console.WriteLine(validation.ValidateEmail("sahiba1214@gmail.com"));
+                        break;
+                    case 4:
+                        Console.WriteLine(validation.ValidateMobile("91 7004830003"));
+                        break;
+                    case 5:
+                        Console.WriteLine(validation.ValidatePassword1("sahiba1214"));
+                        break;
+                    case 6:
+                        Console.WriteLine(validation.ValidatePassword2("sahiba1"));
+                        break;
+                    case 7:
+                        Console.WriteLine(validation.ValidatePassword3("sahiba2"));
+                        break;
+                    case 8:
+                        Console.WriteLine(validation.ValidatePassword4("sahiba3"));
+                        break;
+                    case 9:
                         flag = false;
                         break;
-
-
                 }
-            }
+            }            
         }
     }
 }
+                
+            
+
+
+                
 
